bdc558b4c96a375ece1b573264e39c248d3fe126
AltGits <richcrome@gmail.com>
2018-03-12 19:48:18 -0600

    Apparently fixed the parser by allowing there to be an empty list of BFValues

--- Local/Parse.hs
+++ Local/Parse.hs
@@ -5,13 +5,13 @@ import           Local.Types
 import           Text.Parsec
 import           Text.Parsec.Text (Parser)
 
-readBF input = case parse (many parseBF) "BFuck" input of
+readBF input = case parse parseBF "BFuck" input of
                 Right val -> val
 
-parseBF :: Parser BFuckVal
+parseBF :: Parser [BFuckVal]
 -- Ignores everything that cannot be parsed, then tries each possible value
-parseBF = do skipMany $ noneOf "><+-.,[]"
-             choice (parseLoop : singleBF)
+parseBF = do skipMany (noneOf "><+-.,[]")
+             many . choice $ parseLoop : singleBF
 
 singleBF :: [Parser BFuckVal]
 -- Every possible standalone BF command
@@ -27,7 +27,7 @@ singleBF = [ parseBFChar '>' Forward
 
 parseLoop :: Parser BFuckVal
 parseLoop = do _ <- char '['
-               vals <- many parseBF
+               vals <- parseBF
                _ <- char ']'
                return $ Loop vals
 
